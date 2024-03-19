fdb88582e2f15f2aad7e3dfefb23f652aa5be550
VANL1 <91087333+VANL1@users.noreply.github.com>
2024-03-19 22:47:53 +0300

    1

--- hm2.py
+++ hm2.py
@@ -1,55 +1,57 @@
-#Cборщик, хз зачем-_-
+# Cборщик, не знаю зачем -_-
 class collector:
-    pass
+    def buildcar(self, engine, tank, brakes, body, name):
+        return car(engine, tank, brakes, body, name)
 
 
-#Двигатель
+# Двигатель
 class engine:
     def __init__(self, mass, speed, fuel_consumption):
-        self.mass = mass #Масса двигателя
-        self.speed = speed #Максимальная скорость
-        self.fuel_consumption = fuel_consumption #Расход топлива в час
+        self.mass = mass  # Масса двигателя
+        self.speed = speed  # Максимальная скорость
+        self.fuel_consumption = fuel_consumption  # Расход топлива в час
 
 
-#Бак
+# Бак
 class tank:
     def __init__(self, mass, volume):
-        self.mass = mass #Масса бака
-        self.volume = volume #Объём бака
+        self.mass = mass  # Масса бака
+        self.volume = volume  # Объём бака
 
 
-#Тормоза
+# Тормоза
 class brakes:
     def __init__(self, mass, effect):
-        self.mass = mass #Масса тормозов
-        self.effect = effect #Эффективность тормозов
+        self.mass = mass  # Масса тормозов
+        self.effect = effect  # Эффективность тормозов
 
 
-#Кузов
+# Кузов
 class body:
     def __init__(self, mass):
-        self.mass = mass #Масса кузова
+        self.mass = mass  # Масса кузова
 
-#Сама машина
+
+# Сама машина
 class car:
     def __init__(self, name, engine, tank, brakes, body):
-        self.mass = engine.mass + tank.mass + brakes.mass + body.mass #Общяя масса автомобиля
-        self.name = name #Марка машины
-        self.engine = engine #Вызов двигателя
-        self.tank = tank #Вызов бака
-        self.brakes = brakes #Вызов торомозов
-        self.body = body #Вызов кузова
-        self.max_course=(tank.volume*engine.speed)/engine.fuel_consumption #Максимальный запас хода
-        self.brakes_course=(self.mass/brakes.effect)*100 #Торозной путь
-        
+        self.mass = engine.mass + tank.mass + brakes.mass + body.mass  # Общяя масса автомобиля
+        self.name = name  # Марка машины
+        self.engine = engine  # Вызов двигателя
+        self.tank = tank  # Вызов бака
+        self.brakes = brakes  # Вызов торомозов
+        self.body = body  # Вызов кузова
+        self.max_course = (tank.volume * engine.speed) / engine.fuel_consumption  # Максимальный запас хода
+        self.brakes_course = (self.mass / brakes.effect) * 100  # Торозной путь
 
-engine1 = engine(100, 200, 10) #Двигатель с массой 100кг, макс.скоростью 200 км/ч и расходом 10 л/ч
-engine2 = engine(100, 300, 10) #Двигатель №2 с массой 100, макс.скоростью 300 км/ч и расходом 10 л/ч
-tank1 = tank(100, 50) #Бак с массой 100кг и объёмом 50л
-brakes1 = brakes(100, 100) #Тормоза массой 100кг и эффективностью 100(т/м???)
-body1 = body(100) #Кузов массой 100
-car1 = car("Toyoto", engine1, tank1, brakes1, body1) #Машина №1 марки Toyoto
-car2 = car("BMW", engine2, tank1, brakes1, body1) #Машина №2 марки BMW
+
+engine1 = engine(100, 200, 10)  # Двигатель с массой 100кг, макс.скоростью 200 км/ч и расходом 10 л/ч
+engine2 = engine(100, 300, 10)  # Двигатель №2 с массой 100, макс.скоростью 300 км/ч и расходом 10 л/ч
+tank1 = tank(100, 50)  # Бак с массой 100кг и объёмом 50л
+brakes1 = brakes(100, 100)  # Тормоза массой 100кг и эффективностью 100(т/м???)
+body1 = body(100)  # Кузов массой 100
+car1 = car("Toyoto", engine1, tank1, brakes1, body1)  # Машина №1 марки Toyoto
+car2 = car("BMW", engine2, tank1, brakes1, body1)  # Машина №2 марки BMW
 
 print('Марка:', car1.name)
 print("Масса:", car1.mass)

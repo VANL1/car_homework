111c1e8c08df4e32d52336f7df68afef47d3c6fd
VANL1 <91087333+VANL1@users.noreply.github.com>
2025-03-19 14:09:55 +0300

    Update hm2.py

--- hm2.py
+++ hm2.py
@@ -1,69 +1,87 @@
-# Cборщик, не знаю зачем -_-
-class collector:
+class Collector:
     @staticmethod
-    def buildcar(name, engine, tank, brakes, body):
-        return car(name, engine, tank, brakes, body)
+    def build_car(name, engine, tank, brakes, body):
+        """Создает и возвращает объект автомобиля."""
+        return Car(name, engine, tank, brakes, body)
 
 
 # Двигатель
-class engine:
-    def __init__(self, mass, speed, fuel_consumption):  # Просто вызов функции
+class Engine:
+    def __init__(self, mass, speed, fuel_consumption):
+        """Инициализация двигателя."""
         self.mass = mass  # Масса двигателя
         self.speed = speed  # Максимальная скорость
         self.fuel_consumption = fuel_consumption  # Расход топлива в час
 
 
 # Бак
-class tank:
-    def __init__(self, mass, volume):  # Просто ещё один вызов функции
+class Tank:
+    def __init__(self, mass, volume):
+        """Инициализация бака."""
         self.mass = mass  # Масса бака
         self.volume = volume  # Объём бака
 
 
 # Тормоза
-class brakes:
-    def __init__(self, mass, effect):  # И опять вызов функции
+class Brakes:
+    def __init__(self, mass, effect):
+        """Инициализация тормозов."""
         self.mass = mass  # Масса тормозов
         self.effect = effect  # Эффективность тормозов
 
 
 # Кузов
-class body:
-    def __init__(self, mass):  # И снова вызов функции
+class Body:
+    def __init__(self, mass):
+        """Инициализация кузова."""
         self.mass = mass  # Масса кузова
 
 
-# Сама машина
-class car:
-    def __init__(self, name, engine, tank, brakes, body):  # Угадайте что? Да, вызов функции...
-        self.mass = engine.mass + tank.mass + brakes.mass + body.mass  # Общяя масса автомобиля
+# Автомобиль
+class Car:
+    def __init__(self, name, engine, tank, brakes, body):
+        """Инициализация автомобиля."""
         self.name = name  # Марка машины
-        self.engine = engine  # Вызов двигателя
-        self.tank = tank  # Вызов бака
-        self.brakes = brakes  # Вызов торомозов
-        self.body = body  # Вызов кузова
-        self.max_course = (tank.volume * engine.speed) / engine.fuel_consumption  # Максимальный запас хода
-        self.brakes_course = (self.mass / brakes.effect) * 100  # Торозной путь
+        self.engine = engine  # Двигатель
+        self.tank = tank  # Бак
+        self.brakes = brakes  # Тормоза
+        self.body = body  # Кузов
+        self.mass = self.calculate_total_mass()  # Общая масса автомобиля
+        self.max_course = self.calculate_max_course()  # Максимальный запас хода
+        self.brakes_course = self.calculate_braking_distance()  # Тормозной путь
+
+    def calculate_total_mass(self):
+        """Вычисляет общую массу автомобиля."""
+        return self.engine.mass + self.tank.mass + self.brakes.mass + self.body.mass
+
+    def calculate_max_course(self):
+        """Вычисляет максимальный запас хода."""
+        return (self.tank.volume * self.engine.speed) / self.engine.fuel_consumption
+
+    def calculate_braking_distance(self):
+        """Вычисляет тормозной путь."""
+        return (self.mass / self.brakes.effect) * 100
+
+    def __str__(self):
+        """Возвращает строковое представление автомобиля."""
+        return (f"Марка: {self.name}\n"
+                f"Масса: {self.mass} кг\n"
+                f"Макс. запас хода: {self.max_course} км\n"
+                f"Тормозной путь: {self.brakes_course} м")
 
 
-name1 = "Toyoto"  # Первая марка
-name2 = "BMW"  # Вторая марка
-engine1 = engine(100, 200, 10)  # Двигатель с массой 100кг, макс.скоростью 200 км/ч и расходом 10 л/ч
-engine2 = engine(150, 300, 9)  # Двигатель №2 с массой 100, макс.скоростью 300 км/ч и расходом 10 л/ч
-tank1 = tank(100, 50)  # Бак с массой 100кг и объёмом 50л
-brakes1 = brakes(100, 100)  # Тормоза массой 100кг и эффективностью 100(т/м???)
-body1 = body(100)  # Кузов массой 100
-car1 = car(name1, engine1, tank1, brakes1, body1)  # Машина №1 марки Toyoto
-car2 = car(name2, engine2, tank1, brakes1, body1)  # Машина №2 марки BMW
+# Создание компонентов для автомобилей
+engine1 = Engine(100, 200, 10)  # Двигатель с массой 100кг, макс.скоростью 200 км/ч и расходом 10 л/ч
+engine2 = Engine(150, 300, 9)  # Двигатель №2 с массой 150кг, макс.скоростью 300 км/ч и расходом 9 л/ч
+tank1 = Tank(100, 50)  # Бак с массой 100кг и объёмом 50л
+brakes1 = Brakes(100, 100)  # Тормоза массой 100кг и эффективностью 100
+body1 = Body(100)  # Кузов массой 100кг
 
-print(collector.buildcar(name1, engine1, tank1, brakes1, body1))  # Попытка вызова сборщика (Сполейр не удачная)
+# Создание автомобилей
+car1 = Collector.build_car("Toyota", engine1, tank1, brakes1, body1)
+car2 = Collector.build_car("BMW", engine2, tank1, brakes1, body1)
 
-print('Марка:', car1.name)
-print("Масса:", car1.mass)
-print("Макс запас хода:", car1.max_course)
-print("Тормозной путь:", car1.brakes_course)
-print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")  # Тут весь вывод с описанием (можно поигарть со значениями )
-print('Марка:', car2.name)
-print("Масса:", car2.mass)
-print("Макс запас хода:", car2.max_course)
-print("Тормозной путь:", car2.brakes_course)
+# Вывод информации об автомобилях
+print(car1)
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print(car2)
